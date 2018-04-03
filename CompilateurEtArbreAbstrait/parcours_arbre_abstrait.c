f0440e70ed8023d9dcd9b27e399c253eb699f65d
moonlight83340 <moonlight83340@users.noreply.github.com>
2018-04-03 08:44:01 +0200

    Add files via upload

--- CompilateurEtArbreAbstrait/parcours_arbre_abstrait.c
+++ CompilateurEtArbreAbstrait/parcours_arbre_abstrait.c
@@ -3,12 +3,13 @@
 #include "util.h"
 #include "parcours_arbres_abstrait.h"
 
-int trace_abs = 1;
+extern int portee = P_VARIABLE_GLOBALE;
+extern int adresseLocaleCourante = 0;
+extern int adresseArgumentCourant = 0;
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_n_prog(n_prog *n){
-  char *fct = "prog";
   parcours_l_dec(n->variables);
   parcours_l_dec(n->fonctions); 
   
@@ -19,10 +20,9 @@ void parcours_n_prog(n_prog *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_l_instr(n_l_instr *n){
-  char *fct = "l_instr";
   if(n){ 
-  parcours_instr(n->tete);
-  parcours_l_instr(n->queue); 
+	  parcours_instr(n->tete);
+	  parcours_l_instr(n->queue); 
   }
 }
 
@@ -43,7 +43,6 @@ void parcours_instr(n_instr *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_instr_si(n_instr *n){  
-  char *fct = "instr_si";
   parcours_exp(n->u.si_.test);
   parcours_instr(n->u.si_.alors);
   if(n->u.si_.sinon){
@@ -54,7 +53,6 @@ void parcours_instr_si(n_instr *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_instr_tantque(n_instr *n){
-  char *fct = "instr_tantque";
   parcours_exp(n->u.tantque_.test);
   parcours_instr(n->u.tantque_.faire);
 }
@@ -62,7 +60,6 @@ void parcours_instr_tantque(n_instr *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_instr_affect(n_instr *n){
-  char *fct = "instr_affect";
   parcours_var(n->u.affecte_.var);
   parcours_exp(n->u.affecte_.exp); 
 }
@@ -70,35 +67,39 @@ void parcours_instr_affect(n_instr *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_instr_appel(n_instr *n){
-  char *fct = "instr_appel";
   parcours_appel(n->u.appel); 
 }
 /*-------------------------------------------------------------------------*/
 
 void parcours_appel(n_appel *n){
-  char *fct = "appel"; 
-  parcours_texte( n->fonction, trace_abs);
-  parcours_l_exp(n->args); 
+	int fonc_id = rechercheDeclarative(n->fonction);
+	if (fonc_id >= 0){
+		entreeFonction();
+		parcours_l_exp(n->args);  // ATTENTION : après avoir traité les arguments, votre programme doit modifier __excplicitement___
+		tabsymboles.base = tabsymboles.sommet;  // la valeur de `portee` pour stocker les var. locales avec la portée P_VARIABLE_LOCALE
+		portee = P_VARIABLE_LOCALE;
+		sortieFonction(0);
+		portee = P_VARIABLE_GLOBALE;
+	}else{
+		printf("Nom de fonction inconnue : %s\n", n->fonction);
+	}
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_instr_retour(n_instr *n){
-  char *fct = "instr_retour"; 
   parcours_exp(n->u.retour_.expression);
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_instr_ecrire(n_instr *n){
-  char *fct = "instr_ecrire";
   parcours_exp(n->u.ecrire_.expression);
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_l_exp(n_l_exp *n){
-  char *fct = "l_exp";
   if(n){
     parcours_exp(n->tete);
     parcours_l_exp(n->queue);
@@ -118,22 +119,11 @@ void parcours_exp(n_exp *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_varExp(n_exp *n){
-  char *fct = "varExp"; 
   parcours_var(n->u.var);  
 }
 
 /*-------------------------------------------------------------------------*/
 void parcours_opExp(n_exp *n){
-  char *fct = "opExp";
-  if(n->u.opExp_.op == plus) parcours_texte("plus", trace_abs);
-  else if(n->u.opExp_.op == moins) parcours_texte("moins", trace_abs);
-  else if(n->u.opExp_.op == fois) parcours_texte("fois", trace_abs);
-  else if(n->u.opExp_.op == divise) parcours_texte("divise", trace_abs);
-  else if(n->u.opExp_.op == egal) parcours_texte("egal", trace_abs);
-  else if(n->u.opExp_.op == inferieur) parcours_texte("inf", trace_abs);
-  else if(n->u.opExp_.op == ou) parcours_texte("ou", trace_abs);
-  else if(n->u.opExp_.op == et) parcours_texte("et", trace_abs);
-  else if(n->u.opExp_.op == non) parcours_texte("non", trace_abs);  
   if( n->u.opExp_.op1 != NULL ) {
     parcours_exp(n->u.opExp_.op1);
   }
@@ -145,27 +135,23 @@ void parcours_opExp(n_exp *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_intExp(n_exp *n){
-  char texte[ 50 ]; // Max. 50 chiffres
-  sprintf(texte, "%d", n->u.entier);
-  parcours_element( "intExp", texte, trace_abs );
+
 }
 
 /*-------------------------------------------------------------------------*/
 void parcours_lireExp(n_exp *n){
-  char *fct = "lireExp";
+
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_appelExp(n_exp *n){
-  char *fct = "appelExp"; 
   parcours_appel(n->u.appel);
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_l_dec(n_l_dec *n){
-  char *fct = "l_dec";
   if( n ){   
     parcours_dec(n->tete);
     parcours_l_dec(n->queue);   
@@ -191,26 +177,47 @@ void parcours_dec(n_dec *n){
 /*-------------------------------------------------------------------------*/
 
 void parcours_foncDec(n_dec *n){
-  char *fct = "foncDec"; 
-  parcours_texte( n->nom, trace_abs );
-  parcours_l_dec(n->u.foncDec_.param);
-  parcours_l_dec(n->u.foncDec_.variables);
-  parcours_instr(n->u.foncDec_.corps);
-  
+	int fonc_id = rechercheDeclarative(n->nom);
+	if (fonc_id >= 0){
+		printf("Nom de fonction deja utilise : %s\n", n->nom);
+	}else{
+		// Ajout de la fonction
+		adresseLocaleCourante = 0;
+		tabsymboles.base = tabsymboles.sommet;
+		ajouteIdentificateur(n->nom, portee, T_FONCTION, adresseLocaleCourante, 0); // TODO : nombre de parametres en dernier champ
+	}
+	parcours_l_dec(n->u.foncDec_.param);
+	parcours_l_dec(n->u.foncDec_.variables);
+	parcours_instr(n->u.foncDec_.corps);
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_varDec(n_dec *n){
-  parcours_element("varDec", n->nom, trace_abs);
+	int var_id = rechercheDeclarative(n->nom); // On cherche si "nom" existe deja
+	if (var_id >= 0){
+	// Verifier qu'elles n'ont pas la meme portee   
+
+	}else{
+	// Ajout de la variable
+	ajouteIdentificateur(n->nom, portee, T_ENTIER, adresseLocaleCourante, 1);
+	adresseLocaleCourante += 4;
+	}
 }
 
 /*-------------------------------------------------------------------------*/
 
 void parcours_tabDec(n_dec *n){
-  char texte[100]; // Max. 100 chars nom tab + taille
-  sprintf(texte, "%s[%d]", n->nom, n->u.tabDec_.taille);
-  parcours_element( "tabDec", texte, trace_abs );
+	int var_id = rechercheDeclarative(n->nom);
+	if (var_id >= 0)
+	{
+
+	}else{
+	// Verifier qu'elles n'ont pas la meme portee
+	int id =  ajouteIdentificateur(n->nom, portee, T_TABLEAU_ENTIER, 
+						 adresseLocaleCourante, n->u.tabDec_.taille);
+	adresseLocaleCourante += 4*(n->u.tabDec_.taille);
+	}
 }
 
 /*-------------------------------------------------------------------------*/
@@ -226,13 +233,22 @@ void parcours_var(n_var *n){
 
 /*-------------------------------------------------------------------------*/
 void parcours_var_simple(n_var *n){
-  parcours_element("var_simple", n->nom, trace_abs);
+	int var_id = rechercheExecutable(n->nom); // On cherche si "nom" existe
+	if (id >= 0){ // Si on trouve var_id
+
+	}else{
+		printf("Variable non declaree : %s\n", n->nom);
+	}
 }
 
 /*-------------------------------------------------------------------------*/
 void parcours_var_indicee(n_var *n){
-  char *fct = "var_indicee";
-  parcours_element("var_base_tableau", n->nom, trace_abs);
+	int var_id = rechercheExecutable(n->nom); // On cherche si "nom" existe
+	if (id >= 0){ // Si on trouve var_id
+
+	}else{
+		printf("Variable non declaree : %s\n", n->nom);
+	}
   parcours_exp( n->u.indicee_.indice );
 }
 /*-------------------------------------------------------------------------*/
