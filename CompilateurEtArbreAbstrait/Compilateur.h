2f32bd91665919f3f73de9b490df1a264964f0bd
moonlight83340 <moonlight83340@users.noreply.github.com>
2018-04-21 13:32:56 +0200

    Add files via upload

--- CompilateurEtArbreAbstrait/Compilateur.h
+++ CompilateurEtArbreAbstrait/Compilateur.h
@@ -9,4 +9,12 @@ void readFile(char *file);
 
 void analyseur_lexical(char *file);
 
+void analyseur_syntaxique(char *arg, int verbose);
+
+void analyseur_syntaxique_abs(char *arg);
+
+void table_symboles(char *arg);
+
+void genere_code_intel(char *arg);
+
 #endif
